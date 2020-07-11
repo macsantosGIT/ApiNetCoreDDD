1c9be72b6f1f4a8344f07963f7967629047613a1
Marcelo Correa dos Santos <ma_csantos@hotmail.com>
2020-07-11 12:59:54 -0300

    Migração da versão 3.0 para 3.1 do DotNetCore

--- Api.Application/Startup.cs
+++ Api.Application/Startup.cs
@@ -74,7 +74,7 @@ namespace Application
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1",
 					new OpenApiInfo {
-						Title = "Curso de AspNetCore 3.0",
+						Title = "Curso de AspNetCore 3.1 - 2020",
 						Version = "v1",
 						Description = "Exemplo de API REST criada com o ASP.NET Core",
 						Contact = new OpenApiContact {
@@ -119,7 +119,7 @@ namespace Application
             app.UseSwaggerUI (c => {
                 c.RoutePrefix = string.Empty;
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", 
-                "Curso de API com AspNetCore 3.0");
+                "Curso de API com AspNetCore 3.1");
             });
 
             //Redireciona para swagger, quando acessar rota principal
